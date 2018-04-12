cd5e0e88b6511625618a2c95099d3d42ce80db5e
Cameron Brown <cameron@cmrn.io>
2018-04-12 22:41:29 +1000

    it actually works?

--- src/nn.cpp
+++ src/nn.cpp
@@ -16,11 +16,6 @@ NeuralNetwork::NeuralNetwork(int in, int hid, int out)
     m_inputWeights->randomize();
     m_hiddenWeights->randomize();
 
-    printf("Input Weights:\n");
-    m_inputWeights->print("%.2f ");
-    printf("Hidden Weights:\n");
-    m_hiddenWeights->print("%.2f ");
-
     m_inputBias = new Matrix(hid, 1);
     m_hiddenBias = new Matrix(out, 1);
 
@@ -29,6 +24,8 @@ NeuralNetwork::NeuralNetwork(int in, int hid, int out)
         (*m_inputBias)[i][0] = 1.0f;
     for(int i = 0; i < out; ++i)
         (*m_hiddenBias)[i][0] = 1.0f;
+
+    m_learningRate = 0.1f;
 }
 
 NeuralNetwork::~NeuralNetwork()
@@ -62,7 +59,7 @@ float* NeuralNetwork::feedForward(Matrix& input)
     return result;
 }
 
-float* NeuralNetwork::feedForward(float* input)
+float* NeuralNetwork::guess(float* input)
 {
     // make a matrix from this array
     Matrix m(m_inputNodes, 1);
@@ -73,8 +70,79 @@ float* NeuralNetwork::feedForward(float* input)
     return feedForward(m);
 }
 
+void NeuralNetwork::teach(float* inputs, float* targets)
+{
+    // turn targets into a matrix
+    Matrix targetMatrix(m_outputNodes, 1);
+    for(int i = 0; i < m_outputNodes; ++i)
+        targetMatrix[i][0] = targets[i];
+
+    // turn input into matrix
+    Matrix inputMatrix(m_inputNodes, 1);
+    for(int i = 0; i < m_inputNodes; ++i)
+        inputMatrix[i][0] = inputs[i];
+
+    // grab the current output of the input
+    // not just calling guess because we want to keep track of
+    // the matrices along the way
+    auto hiddenLayer = m_inputWeights->product(inputMatrix);
+    hiddenLayer += *m_inputBias;
+    // apply activation function
+    hiddenLayer.map(&sigmoid);
+
+    // get outputs for output nodes
+    auto outputLayer = m_hiddenWeights->product(hiddenLayer);
+    outputLayer += *m_hiddenBias;
+    // apply activation function
+    outputLayer.map(&sigmoid);
+
+    // start back propagation
+
+    // get the difference between these
+    Matrix outputError = targetMatrix - outputLayer;
+
+    // get the gradient for use in delta calculations
+    Matrix outputGradient = outputLayer;
+    outputGradient.map(&isigmoid);
+    outputGradient *= outputError;
+    outputGradient *= m_learningRate;
+
+    // update bias before we change gradient any more
+    (*m_hiddenBias) += outputGradient;
+
+    // get the deltas to adjust the weights
+    Matrix hiddenLayerT = hiddenLayer.transposed();
+    outputGradient = outputGradient.product(hiddenLayerT);
+    (*m_hiddenWeights) += outputGradient;
+
+    // grab hidden layer error
+    Matrix hiddenTrans = m_hiddenWeights->transposed();
+    Matrix hiddenError = hiddenTrans.product(outputError);
+
+    // get hidden gradient
+    Matrix hiddenGradient = hiddenLayer;
+    hiddenGradient.map(&isigmoid);
+    hiddenGradient *= hiddenError;
+    hiddenGradient *= m_learningRate;
+
+    // adjust bias
+    (*m_inputBias) += hiddenGradient;
+
+    // get the deltas for these weights
+    Matrix inputLayerT = inputMatrix.transposed();
+    hiddenGradient = hiddenGradient.product(inputLayerT);
+    (*m_inputWeights) += hiddenGradient;
+}
+
 float sigmoid(float x)
 {
     float ex = expf(x);
     return ex / (ex + 1);
+}
+
+// inverse of sigmoid
+// but we're assuming n has already been mapped using sigmoid
+float isigmoid(float n)
+{
+    return n * (1 - n);
 }
--- src/nn.hpp
+++ src/nn.hpp
@@ -3,19 +3,27 @@
 class Matrix;
 
 float sigmoid(float x);
+float isigmoid(float n);
 
 class NeuralNetwork {
 public:
     NeuralNetwork(int in, int hid, int out);
     ~NeuralNetwork();
 
-    float* feedForward(float* input);
+    float* guess(float* input);
     float* feedForward(Matrix& input);
+
+    void teach(float* inputs, float* targets);
+
+    float getLearningRate() { return m_learningRate; }
+    void setLearningRate(float rate) { m_learningRate = rate; }
 
 private:
     int m_inputNodes;
     int m_hiddenNodes;
     int m_outputNodes;
+
+    float m_learningRate;
 
     Matrix* m_inputWeights;
     Matrix* m_hiddenWeights;
