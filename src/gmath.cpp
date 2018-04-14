b0cd26952299fc5dff5ad818e625d85e8233dd69
Cameron Brown <cameron@cmrn.io>
2018-04-14 16:52:56 +1000

    add some comments to math stuff

--- src/gmath.cpp
+++ src/gmath.cpp
@@ -13,10 +13,13 @@ float randBetween(float min, float max)
 
 float map(float v, float vMin, float vMax, float oMin, float oMax)
 {
+    // get the difference between the minimums and maximums
     float vDif = vMax - vMin;
     float oDif = oMax - oMin;
 
+    // get the percentage of the value between minimum and maximum
     float perc = (v - vMin) / vDif;
 
+    // apply that percentage to the new range
     return oMin + (perc * oDif);
 }
--- src/gmath.h
+++ src/gmath.h
@@ -7,7 +7,24 @@ inline int sign(int n) { return n < 0 ? -1 : 1; }
 
 inline float absf(float n) { return n < 0 ? -n : n; }
 
+/***
+ * @brief Returns a random float in a specified range
+ * @param min Minimum value of the random number
+ * @param max Maximum value of the random number
+ * @return A random number within the range
+ */
 float randBetween(float min, float max);
 
+/***
+ * @brief Takes a value and its range and then scales it to fit another range
+ *          Works like Processing's map function:
+ *          https://processing.org/reference/map_.html
+ * @param v Value to scale
+ * @param vMin Original minimum value
+ * @param vMax Original maxmimum value
+ * @param oMin New minimum value
+ * @param oMax New maximum value
+ * @return The scaled number
+ */
 float map(float v, float vMin, float vMax, float oMin, float oMax);
 
